1fd15708ad9ffe34099cd800b17d82ded0701e58
Dadidaderp <lauzm799@gmail.com>
2016-06-13 18:09:56 +0200

    Commit Marc
    Debug

--- nissan.php
+++ nissan.php
@@ -15,13 +15,14 @@
 		<input type="button" name="inscription.php" value="Inscription" onclick="self.location.href='inscription.html'" style="background-color:#3cb371" style="color:white; font-weight:bold"onclick> <input type="submit" value"Se connecter">
 	</div>
 	
-	<ul id="menu-demo2">
+<ul id="menu-demo2">
 	<li><a href="menu_modeles.php">Location</a>
 		<ul>
 			<li><a href="audi.php">Audi</a></li>
 			<li><a href="bmw.php">BMW</a></li>
 			<li><a href="mini.php">Mini</a></li>
 			<li><a href="nissan.php">Nissan</a></li>
+		</ul>
 	</li>
 	<li><a href="#">A propos</a>
 		<ul>
